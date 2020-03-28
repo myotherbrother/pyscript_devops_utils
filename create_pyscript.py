1a40c24c9a3f25c78a2ca825ea04ca0099dee452
Darrel Gill <darrel.gill@outlook.com>
2020-03-28 15:21:12 +0000

    add create_pyscript.py

---
+++ create_pyscript.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+directory=sys.argv[1]
+filename=sys.argv[2]
+def create_pyscript(directory, filename):
+    #this function creates a new pyscript file, adds the inerpretur and opens it in vim.
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+    os.chdir(directory)
+    envpy3 = "#!/usr/bin/env python3\n"
+    fullname = os.path.join(directory, filename)
+    if not os.path.exists(fullname):
+        with open(fullname, "w") as open_file:
+            open_file.write(envpy3)
+    else:
+        print("~~ERROR!~~, {} already exists!".format(fullname))
+        sys.exit(1)
+
+
+create_pyscript(sys.argv[1], sys.argv[2])
