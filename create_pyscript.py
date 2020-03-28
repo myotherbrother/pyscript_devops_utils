f4d1a28a5f88800a32b613efd70cbf57295f902a
Darrel Gill <darrel.gill@outlook.com>
2020-03-28 16:33:10 +0000

    add def edit_perms() to update new script file permissions to 0775

--- create_pyscript.py
+++ create_pyscript.py
@@ -2,8 +2,9 @@
 
 import os
 import sys
-directory=sys.argv[1]
-filename=sys.argv[2]
+import stat
+#directory=sys.argv[1]
+#filename=sys.argv[2]
 def create_pyscript(directory, filename):
     #this function creates a new pyscript file, adds the interpreter.
     if not os.path.isdir(directory):
@@ -19,5 +20,17 @@ def create_pyscript(directory, filename):
         print("~~ERROR!~~, {} already exists!".format(fullname))
         sys.exit(1)
 
+    return fullname
 
+def edit_perms(fullname):
+    # changes permissions on newly created script file using chmod +x
+    os.chmod(fullname, 0o775) 
+    
+
+if __name__ == "__main__":
+    directory = sys.argv[1]
+    filename = sys.argv[2]
+    fullname = create_pyscript(directory, filename)
+    edit_perms(fullname)
+    sys.exit(0)
 create_pyscript(sys.argv[1], sys.argv[2])
